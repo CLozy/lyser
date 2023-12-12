38a32b141a3e96ba10e5816e4a6fddd40fd3a7fd
Charleenlozi <clozymwangs@gmail.com>
2023-12-12 06:49:00 +0300

    feat: packaging the class

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lyser',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={},
+    test_suite='tests',
+)
