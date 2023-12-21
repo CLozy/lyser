0cc73e4cf37ebd69de22c78989f70a670fd0013b
Charleenlozi <clozymwangs@gmail.com>
2023-12-21 12:12:36 +0300

    feat: adding recipes

--- lyser/chef_chat.py
+++ lyser/chef_chat.py
@@ -1,5 +1,6 @@
 
 import os
+import getpass
 from dotenv import load_dotenv
 
 from fastapi import FastAPI
@@ -7,8 +8,12 @@ from fastapi import FastAPI
 
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage
+from langchain.document_loaders import TextLoader, PyPDFLoader
+
 
 load_dotenv()
-
+os.environ["OPENAI_API_KEY"] = getpass.getpass()
 
 chat = ChatOpenAI()
+
+
